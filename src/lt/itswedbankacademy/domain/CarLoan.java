916a1e79b6dc62530cd4d799e344beb40b7f0f77
andrewmic <37101387+andrewmic@users.noreply.github.com>
2018-03-15 09:06:52 +0200

    Review notes.

--- src/lt/itswedbankacademy/domain/CarLoan.java
+++ src/lt/itswedbankacademy/domain/CarLoan.java
@@ -17,6 +17,12 @@ public class CarLoan extends VehicleLoan implements Comparable<CarLoan> {
     @Override
     public int compareTo(CarLoan o) {
 
+        //First of all, why are you comparing "price" 3 times?
+        //Second, why 2 of these times you are comparing "this" to "that" (ascending) and third (final) time "that" to "this"
+        // descending (correct order)? :) Good approach would be to compare once and re-use saved result later on
+        // (see example below).
+        //Third, do not use "this" keyword if not necessary (Java is not TypeScript). Here is a good explanation when to use "this"
+        // https://stackoverflow.com/questions/2411270/when-should-i-use-this-in-a-class
         int comp = getPrice().compareTo(o.getPrice());
         if (this.getPrice().compareTo(o.getPrice()) == 0) {
 
@@ -30,6 +36,15 @@ public class CarLoan extends VehicleLoan implements Comparable<CarLoan> {
             }
         }
         return -(this.getPrice().compareTo(o.getPrice()));
+
+/*        int compareResult = -getPrice().compareTo(o.getPrice());
+        if (compareResult != 0) {
+            return compareResult;
+        }
+
+        compareResult = -Float.compare(enginePower, o.enginePower);
+
+        return compareResult == 0 ? getInterestRate().compareTo(o.getInterestRate()) : compareResult;*/
     }
 
 }
